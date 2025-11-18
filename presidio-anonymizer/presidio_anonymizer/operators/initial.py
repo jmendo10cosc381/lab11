8d7c55b022e8d1eae259761091108227abb16116
Julia Mendoza <231963277+jmendo10cosc381@users.noreply.github.com>
2025-11-18 19:09:21 +0000

    Updated operate method and created an initial extractor method

--- presidio-anonymizer/presidio_anonymizer/operators/initial.py
+++ presidio-anonymizer/presidio_anonymizer/operators/initial.py
@@ -3,9 +3,26 @@ from typing import Dict
 from presidio_anonymizer.operators import Operator, OperatorType
 
 class Initial(Operator):
+    def extract_initial(self, word: str) -> str:
+        prefix = ""
+
+        for char in word:
+            if char.isalnum():
+                return prefix + char.upper() + "."
+            else:
+                prefix += char
+        
+        return word
+
     def operate(self, text: str = None, params: Dict = None) -> str:
-        """:return: an empty value."""
-        return ""
+        if not text:
+            return ""
+        
+        words = text.strip().split()
+
+        initials = [self.extract_initial(w) for w in words]
+        
+        return " ".join(initials)
 
     def validate(self, params: Dict = None) -> None:
         """Redact does not require any parameters so no validation is needed."""
@@ -18,4 +35,5 @@ class Initial(Operator):
     def operator_type(self) -> OperatorType:
         """Return operator type."""
         return OperatorType.Anonymize
+
 
